16de89dde2dfda837b2636e7ee019e3581681c54
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-12-27 13:57:53 -0500

    Initial Commit

---
+++ HealthAppProject/HealthAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthAppProjectApp.swift
+//  HealthAppProject
+//
+//  Created by Kevin Mattocks on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
