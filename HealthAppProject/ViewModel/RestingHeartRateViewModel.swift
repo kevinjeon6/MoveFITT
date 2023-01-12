f695f0ddccfafc990b8885ff90b6c364165b154b
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2023-01-11 19:18:48 -0500

    Create model for RestingHeartRate Create view model for Resting heart rate. Made separate to avoid clutter in HealthStoreViewModel

---
+++ HealthAppProject/ViewModel/RestingHeartRateViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  RestingHeartRateViewModel.swift
+//  HealthAppProject
+//
+//  Created by Kevin Mattocks on 1/11/23.
+//
+
+import Foundation
+import HealthKit
+
+class RestingHeartRateViewModel: ObservableObject {
+ 
+    
+    
+}
