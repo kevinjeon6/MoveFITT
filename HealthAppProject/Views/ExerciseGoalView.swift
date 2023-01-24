218c60b97342db8bbc3d258918227b69bca7007b
Kevin <kevin_jeon@icloud.com>
2023-01-24 11:08:39 -0500

    Update properties in ExerciseGoalView to be dynamic Add a showInfoSheet and button which will show the benefits of exercise Adjust and update layout

--- HealthAppProject/Views/ExerciseGoalView.swift
+++ HealthAppProject/Views/ExerciseGoalView.swift
@@ -9,52 +9,74 @@ import SwiftUI
 
 struct ExerciseGoalView: View {
     
-    var progress = 80.0
-    private let minValue = 0.0
-    private let maxValue = 150.0
+    var progress: Double
+    var minValue: Double
+    var maxValue: Double
+    var description: String
+    @State private var showInfoSheet = false
     
     var body: some View {
-      
-            
+        
+        
         ZStack {
             RoundedRectangle(cornerRadius: 20)
-            .stroke(.blue.opacity(0.7), lineWidth: 3 )
-            .frame(width: 400, height: 300)
-            .overlay(alignment: .top) {
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 400, height: 150)
-            }
-            
-            
-            VStack(alignment: .leading) {
-                HStack {
-                    Image(systemName: "figure.mixed.cardio")
-                    Text("Exercise Goal")
+                .stroke(.blue.opacity(0.7), lineWidth: 3 )
+                .padding(.horizontal)
+                .frame(width: 400, height: 200)
+                .overlay(alignment: .top) {
+                    RoundedRectangle(cornerRadius: 20)
+                        .padding(.horizontal)
+                        .frame(width: 400, height: 80)
                 }
-                .font(.title2)
-                .foregroundColor(.white)
-                
-                
-                Text("150 min/week")
-                    .font(.title3)
-                    .foregroundColor(.white)
-                
-            }
             
-            VStack {
-                HStack(spacing: 80) {
-                    ExerciseGaugeView()
-                    ExerciseGaugeView()
+            
+            
+            VStack(spacing: 30) {
+                VStack(alignment: .leading, spacing: 5) {
+                    HStack {
+                        Image(systemName: "figure.mixed.cardio")
+                        Text("Exercise Goal")
+                        Spacer()
+                        Button {
+                            showInfoSheet.toggle()
+                        } label: {
+                            Image(systemName: "info.circle")
+                        }
+                        .sheet(isPresented: $showInfoSheet){
+                            InfoView(description: description)
+                                .presentationDetents([.medium])
+                        }
+                        
+                    }
+                    .font(.title2)
+                    .foregroundColor(.blue)
+                    
+                    Text("150 min/week")
+                        .font(.title3)
+                        .foregroundColor(.white)
                 }
+                .padding(.horizontal, 30)
+        
+                
+                VStack{
+                    HStack(spacing: 100) {
+                        
+                        ExerciseGaugeView(progress: progress, minValue: minValue, maxValue: maxValue, title: "Today")
+                        
+                        ExerciseGaugeView(progress: progress, minValue: minValue, maxValue: maxValue, title: "Weekly")
+                    }
+                }
+        
+                
+               
+                
             }
-            .padding(.horizontal, 30)
-            
         }
     }
 }
 
 struct ExerciseGoalView_Previews: PreviewProvider {
     static var previews: some View {
-        ExerciseGoalView()
+        ExerciseGoalView(progress: 75, minValue: 0, maxValue: 100, description: "Text goes Here")
     }
 }
