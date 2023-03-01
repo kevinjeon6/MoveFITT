156bba671dacab8060de0ef44fd4e883b2a4d249
Kevin <kevin_jeon@icloud.com>
2023-02-28 23:14:01 -0500

    Provide descriptions for muscle strength activity and physical activity

--- HealthAppProject/Helpers/HealthTypeInfoText.swift
+++ HealthAppProject/Helpers/HealthTypeInfoText.swift
@@ -18,7 +18,11 @@ struct HealthInfoText {
     }
     
     static var strengthActivityDescription: String {
-        "Performing muscle strengthening exercise twice a week is important"
+        "Performing muscle strengthening exercise 2 times a week or more can provide an increase in muscular strength, muscular power, muscle mass, and bone strnegth. These factors are important to perform activities of daily living. Muscle-strengthening activities should include all major muscle groups - legs, hips, back, chest, shoulders, arms, and abdominals. "
+    }
+    
+    static var physicalActivityDescription: String {
+        "Physical activity is any bodily movement that increases energy expenditure above basal level. It is recommended that individuals perform at least 150 minutes to 300 minutes a week of moderate-intensity or 75 minutes to 150 minutes a week of vigorous-intensity of aerobic activity. "
     }
     
 }
