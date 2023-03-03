426cead441b124ad1a7cbb28db4c3aeee7d4e977
Kevin <kevin_jeon@icloud.com>
2023-03-02 23:29:30 -0500

    Testing a new UI look for Step data

---
+++ HealthAppProject/Views/CardViews/StepCountTileView.swift
@@ -0,0 +1,43 @@
+//
+//  StepCountTileView.swift
+//  HealthAppProject
+//
+//  Created by Kevin Mattocks on 3/2/23.
+//
+
+import SwiftUI
+
+struct StepCountTileView: View {
+    var currentValue: Int
+    var goalText: Int
+    var stepPercent: Int
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            HStack {
+                Image(systemName: "figure.walk")
+                Text("Steps")
+                Spacer()
+                Image(systemName: "chevron.forward")
+            }
+            
+            Text("\(currentValue)")
+            Text("Goal: \(goalText) steps")
+            HStack {
+                ProgressionStepBar(value: currentValue, goalValue: goalText)
+                Text("\(stepPercent)%")
+            }
+            
+        }
+        .padding()
+        .frame(width: 200)
+        .cardBackground()
+ 
+    }
+}
+
+struct StepCountTileView_Previews: PreviewProvider {
+    static var previews: some View {
+        StepCountTileView(currentValue: 7_000, goalText: 10_000, stepPercent: 70)
+    }
+}
