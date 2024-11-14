660aacb211be56a2ae1ad2cf727eb49ebed228a9
Kevin Mattocks <80538229+kevinjeon6@users.noreply.github.com>
2024-11-14 18:20:14 -0500

    Updated logic UI for Health Metric Snapshot

--- HealthAppProject/Views/HealthMetricSnapShotView.swift
+++ HealthAppProject/Views/HealthMetricSnapShotView.swift
@@ -10,26 +10,27 @@ import SwiftUI
 struct HealthMetricSnapShotView: View {
     var body: some View {
         NavigationStack {
-            VStack(alignment: .leading) {
+            VStack {
                 NavigationLink {
                     //Go to charts of the HealthMetric
                 } label: {
                     HStack {
-                        VStack(alignment: .leading) {
                             Text("Heart Overview")
                                 .font(.title3)
-                        }
+                        
                         Spacer()
                         Image(systemName: "chevron.right")
                     }
+                    .padding(.bottom, 4)
                 }
                 .foregroundStyle(.secondary)
+
                 Divider()
-                
-                HStack {
-                    VStack(alignment: .leading, spacing: 5) {
-                        
-                        VStack(alignment: .leading, spacing: 10) {
+
+                Grid(alignment: .leading, horizontalSpacing: 80, verticalSpacing: 20) {
+                    GridRow {
+                        // MARK: - Make into a reusable view
+                        VStack(alignment: .leading, spacing: 5) {
                             HStack {
                                 Image(systemName: "arrow.down.heart.fill")
                                 Text("RHR")
@@ -39,12 +40,8 @@ struct HealthMetricSnapShotView: View {
                             Text("62 bpm")
                         }
                         .font(.title2)
-                        .padding(.bottom, 20)
                         
-                        
-                        
-                        
-                        VStack(alignment: .leading, spacing: 10) {
+                        VStack(alignment: .leading, spacing: 5) {
                             HStack {
                                 Image(systemName: "heart.circle.fill")
                                 Text("HR")
@@ -55,16 +52,12 @@ struct HealthMetricSnapShotView: View {
                             Text("74 bpm")
                         }
                         .font(.title2)
-                        
-                    }
-                    .font(.callout)
 
-                    Spacer()
+                    }
                     
-                    
-                    VStack(alignment: .leading, spacing: 5) {
-                        
-                        VStack(alignment: .leading, spacing: 10) {
+                    GridRow {
+                        // MARK: - Make into a reusable view
+                        VStack(alignment: .leading, spacing: 5) {
                             HStack {
                                 Image(systemName: "waveform.path.ecg.rectangle.fill")
                                 Text("HRV")
@@ -74,33 +67,26 @@ struct HealthMetricSnapShotView: View {
                             Text("220 ms")
                         }
                         .font(.title2)
-                        .padding(.bottom, 20)
                         
-                        
-                        
-                        VStack(alignment: .leading, spacing: 10) {
+                        VStack(alignment: .leading, spacing: 5) {
                             HStack {
                                 Image(systemName: "heart.circle.fill")
                                 Text("Walking HR")
                                     .font(.subheadline)
                                     .fontWeight(.heavy)
                             }
+                            
                             Text("76 bpm")
+                            
                         }
                         .font(.title2)
-                        
                     }
-                    .font(.callout)
-                    Spacer()
                 }
-                .padding(.top, 15)
-                Spacer()
+                .padding(.top, 20)
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .frame(height: 230)
-            .padding()
+            .padding(20)
             .background(
-                RoundedRectangle(cornerRadius: 14, style: .continuous).fill(Color(.secondarySystemBackground))
+                RoundedRectangle(cornerRadius: 16, style: .continuous).fill(Color(.secondarySystemBackground))
             )
         }
     }
