fbf6aa72068818c6cbb6f82b56024e781077efc6
Kevin <kevin_jeon@icloud.com>
2024-11-20 21:07:23 -0500

    Add ProgressGauge to the WeeklyView for Strength training goal

--- HealthAppProject/Views/WeeklyView.swift
+++ HealthAppProject/Views/WeeklyView.swift
@@ -9,28 +9,58 @@ import SwiftUI
 
 struct WeeklyView: View {
     
+    var progress: Double
+    var minValue: Double
+    var maxValue: Double
+    var title: Int
+    var color: Color
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            Text("Strength Traning")
-            HStack {
-                Text("Goal: 0/2")
-                Image(systemName: "checkmark.circle.fill")
-                    .foregroundStyle(.green)
+        
+        HStack(spacing: 30){
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Strength Traning")
+                    .font(.title2)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.1)
+                
+                HStack {
+                    Text("Goal: 0/2")
+                    Image(systemName: "checkmark.circle.fill")
+                        .foregroundStyle(.green)
+                }
+                .font(.subheadline)
+                .padding(.bottom)
+                
+                Text("Physical Activity")
+                    .font(.title2)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.1)
+                
+                HStack {
+                    Text("200/350")
+                    Image(systemName: "checkmark.circle.fill")
+                        .foregroundStyle(.green)
+                }
+                .font(.subheadline)
             }
-            .padding(.bottom)
             
-            Text("Physical Activity")
-            HStack {
-                Text("200/350")
-                Image(systemName: "checkmark.circle.fill")
-                    .foregroundStyle(.green)
-            }
+            Spacer()
+            ProgressGaugeView(
+                progress: progress,
+                minValue: minValue,
+                maxValue: maxValue,
+                scaleValue: 1.9,
+                gaugeColor: color,
+                title: title)
+            
+            Spacer()
         }
+        .padding(.bottom, 32)
         .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(.bottom, 25)
     }
 }
 
 #Preview {
-    WeeklyView()
+    WeeklyView(progress: 2, minValue: 0, maxValue: 3, title: 2, color: .green)
 }
