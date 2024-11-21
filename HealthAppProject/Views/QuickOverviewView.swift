017a60b61fb62f5a6346218827696c5096ffd747
Kevin Mattocks <80538229+kevinjeon6@users.noreply.github.com>
2024-11-21 12:57:07 -0500

    Create separate view for Indicator and change scrollTargetBehavior

--- HealthAppProject/Views/QuickOverviewView.swift
+++ HealthAppProject/Views/QuickOverviewView.swift
@@ -27,7 +27,7 @@ struct QuickOverviewView: View {
                 }
                 .scrollTargetLayout()
             }
-            .scrollTargetBehavior(.paging)
+            .scrollTargetBehavior(.viewAligned)
             .scrollPosition(id: $currentCard)
 
 
@@ -40,26 +40,7 @@ struct QuickOverviewView: View {
 }
 
 
-struct IndicatorView: View {
-    @Binding var currentCard: Int?
 
-    
-    var body: some View {
-        HStack {
-            ForEach(0..<2) { index in
-                    Circle()
-                    .fill(currentCard == index ? .white : Color(.lightGray))
-                    .frame(width: currentCard == index ? 12 : 8, height: 8)
-                    .shadow(radius: 4)
-                    .onTapGesture {
-                        currentCard = index
-                    }
-            }
-        }
-        .padding(5)
-        .background(.thickMaterial, in: Capsule())
-    }
-}
 
 #Preview {
     QuickOverviewView()
