16e36f749cf8d8e377b708711e0e403586a8700e
Kevin <kevin_jeon@icloud.com>
2024-11-21 16:35:13 -0500

    Add colors for health metric view of heart and respiratory

--- HealthAppProject/Extensions/ColorExtension.swift
+++ HealthAppProject/Extensions/ColorExtension.swift
@@ -11,4 +11,20 @@ import SwiftUI
 extension Color {
     static let darkModeColor = Color("Dark-mode")
     static let tangBlue = Color("Tang-Blue")
+    
+    
+    // MARK: - Blues
+    static let lightestBlue = Color(.lightestBlue)
+    static let lightBlue = Color(.lightBlue)
+    static let mediumBlue = Color(.mediumBlue)
+    static let deepBlue = Color(.deepBlue)
+    static let darkBlue = Color(.darkBlue)
+    
+    
+    // MARK: - Reds
+    static let lightestRed = Color(.lightestRed)
+    static let lightRed = Color(.lightRed)
+    static let mediumRed = Color(.mediumRed)
+    static let deepRed = Color(.deepRed)
+    static let darkRed = Color(.darkRed)
 }
